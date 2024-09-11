597bf3aa8384453df2b02be36097fa49538ae994
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-09-11 09:54:32 +0530

    added redux store and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login , logout} from './store/authSlice'
+import { Header ,Footer } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [loading, setLoading] = useState(true)
 
-  return (
-    <>
-        <H1>BLog WIth react </H1>
-    </>
-  )
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  }, [])
+  
+
+
+
+  return !loading ? (
+    <div className='bg-gray-400 min-h-screen flex flex-wrap content-between'>
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
