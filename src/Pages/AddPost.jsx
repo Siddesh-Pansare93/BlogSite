ace5a514b888a923d8cb9db62d422358d97b61d1
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-03-05 12:00:00 +0530

    feat: integrate user data retrieval in AddPost component

--- src/Pages/AddPost.jsx
+++ src/Pages/AddPost.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { PostForm } from '../components'
 import {Container} from '../components'
+import { useSelector } from 'react-redux'
 
 function AddPost() {
+
+  const user = useSelector(state => state.auth.userData)
+  console.log(user);
+  
   return (
     <Container>
         <PostForm />
